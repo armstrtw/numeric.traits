29c79c883a41d2705ef2833b9d739b9b321f26e9
whit <armstrong.whit@gmail.com>
2018-01-26 16:02:53 -0500

    initial add

---
+++ numeric.traits.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <limits>
+#include <cmath>
+
+
+#ifndef __BYTE_ORDER__
+#error Please define __BYTE_ORDER__ to be __ORDER_LITTLE_ENDIAN__ or __ORDER_BIG_ENDIAN__
+#endif 
+
+namespace rnumeric {
+
+  union IEEE754 {
+    unsigned int word[2];
+    double value;
+  };
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+  static const IEEE754 rna { {1954, 0x7ff00000} };
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+  static const IEEE754 rna { {0x7ff00000, 1954} };
+#endif
+
+  template <typename T> class RNT;
+
+  template <> class RNT<double> {
+  public:
+    static const bool has_NA{true};
+    static const double NA{rna.value};
+    static inline bool ISNA(double x) { return std::isnan(x); }
+  };
+
+  template <> class RNT<int> {
+  public:
+    static const bool has_NA {true};
+    static const int NA {std::numeric_limits<int>::min()};
+    static inline bool ISNA(int x) { return x == std::numeric_limits<int>::min() ? true : false; }
+  };
+}
