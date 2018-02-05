c31728f6275fbff645b5d0c3611b5c177ad7ec30
whit <armstrong.whit@gmail.com>
2018-02-05 13:28:41 -0500

    convert to static fun and reformat

--- numeric.traits.hpp
+++ numeric.traits.hpp
@@ -1,38 +1,43 @@
 #pragma once
-#include <limits>
 #include <cmath>
-
+#include <limits>
 
 #ifndef __BYTE_ORDER__
 #error Please define __BYTE_ORDER__ to be __ORDER_LITTLE_ENDIAN__ or __ORDER_BIG_ENDIAN__
-#endif 
+#endif
 
 namespace rnumeric {
 
-  union IEEE754 {
-    unsigned int word[2];
-    double value;
-  };
+union IEEE754 {
+  unsigned int word[2];
+  double value;
+};
 
+template <typename T>
+class RNT;
+
+template <>
+class RNT<double> {
+public:
+  static const bool has_NA{true};
+  static const double NA() {
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-  static const IEEE754 rna { {1954, 0x7ff00000} };
+    static const IEEE754 rna{{ 1954, 0x7ff00000 }};
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-  static const IEEE754 rna { {0x7ff00000, 1954} };
+    static const IEEE754 rna{{0x7ff00000, 1954}};
 #endif
 
-  template <typename T> class RNT;
+    return rna.value;
+  }
 
-  template <> class RNT<double> {
-  public:
-    static const bool has_NA{true};
-    static const double NA{rna.value};
-    static inline bool ISNA(double x) { return std::isnan(x); }
-  };
+  static inline bool ISNA(double x) { return std::isnan(x); }
+};
 
-  template <> class RNT<int> {
-  public:
-    static const bool has_NA {true};
-    static const int NA {std::numeric_limits<int>::min()};
-    static inline bool ISNA(int x) { return x == std::numeric_limits<int>::min() ? true : false; }
-  };
-}
+template <>
+class RNT<int> {
+public:
+  static const bool has_NA{true};
+  static const int NA() { return std::numeric_limits<int>::min(); }
+  static inline bool ISNA(int x) { return x == std::numeric_limits<int>::min() ? true : false; }
+};
+} // namespace rnumeric
